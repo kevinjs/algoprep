aa4da2c0fe8edb1ffd46d038e7e03d6420adeb14
dysj4099@gmail.com <dysj4099@gmail.com>
2015-04-09 13:56:44 +0800

    insertion sort

--- python/sort/insection_sort.py
+++ python/sort/insection_sort.py
@@ -4,7 +4,7 @@ import base
 from base import timeit
 
 @timeit
-def insection_sort(arr):
+def insertion_sort(arr):
     rtn = []
 
     for i in arr:
@@ -30,11 +30,45 @@ def insert(sorted_arr, new_ele):
     else:
 	new_arr[i] = new_ele
 
-    return new_arr 
+    return new_arr
+
+@timeit
+def insertion_sort2(arr):
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i-1]:
+            p = i-1
+            t = arr[i]
+            arr[i] = arr[i-1]
+            while t < arr[p] and p >= 0:
+                arr[p+1] = arr[p]
+                p -= 1
+            arr[p+1] = t
+    return arr
+
+def shell_sort(arr, n):
+    di = n/2
+    while di >=1:
+        insertion_sort_base(arr, n, di)
+        di = di/2
+    return arr
+
+def insertion_sort_base(arr, n, d):
+    for i in range(d, n):
+        if arr[i] < arr[i-d]:
+            p = i-d
+            t = arr[i]
+            arr[i] = arr[i-d]
+            while p >= 0 and t < arr[p]:
+                arr[p+d] = arr[p]
+                p -= d
+            arr[p+d] = t
+    return arr
 
 if __name__=='__main__':
     arr = [5, 21, 4, 6, 12, 33, 15, 195, 27, 31, 57]
 
     base.print_arr(arr)
-    arr = insection_sort(arr)
+    #arr = insection_sort2(arr)
+    #arr = insertion_sort_base(arr, len(arr), 1)
+    arr = shell_sort(arr, len(arr))
     base.print_arr(arr)
